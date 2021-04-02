e60c17e2120c95390a4fc978dbb7904b855fa017
201118063 <201118063@samsun.edu.tr>
2021-04-02 14:40:52 +0300

    World Map Ekleme

---
+++ MVCKutuphane/Controllers/İstatistikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCKutuphane.Controllers
+{
+    public class İstatistikController : Controller
+    {
+        // GET: İstatistik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
