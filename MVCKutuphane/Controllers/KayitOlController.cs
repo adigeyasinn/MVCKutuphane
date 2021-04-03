872c940cdc049e24a1c78f42cd9114e9c5ff6f8f
201118063 <201118063@samsun.edu.tr>
2021-04-03 23:59:32 +0300

    Login Paneli

---
+++ MVCKutuphane/Controllers/KayitOlController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+namespace MVCKutuphane.Controllers
+{
+    public class KayitOlController : Controller
+    {
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+        // GET: KayitOl
+        [HttpGet]
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kayit(TBLUyeler p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Kayit");
+            }
+            db.TBLUyeler.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
