a0ff9b7eda19e5f955c2f93b51b8ea32656973c4
201118063 <201118063@samsun.edu.tr>
2021-04-11 14:50:03 +0300

    Profil Bilgileri Güncelleme

--- MVCKutuphane/Controllers/LoginController.cs
+++ MVCKutuphane/Controllers/LoginController.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+using System.Web.Security;
 
 namespace MVCKutuphane.Controllers
 {
     public class LoginController : Controller
     {
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
         // GET: Login
         public ActionResult GirisYap()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult GirisYap(TBLUyeler p)
+        {
+            var bilgiler = db.TBLUyeler.FirstOrDefault(x => x.MAİL == p.MAİL && x.SIFRE == p.SIFRE);
+            if (bilgiler!=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAİL, false);
+                Session["Mail"] = bilgiler.MAİL.ToString();
+                //TempData["Id"] = bilgiler.ID.ToString();
+                //TempData["Ad"] = bilgiler.AD.ToString();
+                //TempData["Soyad"] = bilgiler.SOYAD.ToString();
+                //TempData["KullanıcıAdı"] = bilgiler.KULLANICIADI.ToString();
+                //TempData["Sifre"] = bilgiler.SIFRE.ToString();
+                //TempData["Okul"] = bilgiler.OKUL.ToString();
+                return RedirectToAction("Index","Panelim");
+            }
+            else
+            {
+                return View();
+            }
+            
         }
     }
 }
--- MVCKutuphane/Controllers/PanelimController.cs
+++ MVCKutuphane/Controllers/PanelimController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MVCKutuphane.Models.Entity;
 namespace MVCKutuphane.Controllers
 {
     public class PanelimController : Controller
     {
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
         // GET: Panelim
+        [HttpGet]
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var uyemail = (string)Session["Mail"];
+            var degerler = db.TBLUyeler.FirstOrDefault(x => x.MAİL == uyemail);
+            return View(degerler);
+        }
+        [HttpPost]
+        public ActionResult Index2(TBLUyeler p)
+        {
+            var kullanici = (string)Session["Mail"];
+            var uye = db.TBLUyeler.FirstOrDefault(x => x.MAİL == kullanici);
+            uye.SIFRE = p.SIFRE;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
